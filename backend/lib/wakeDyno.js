8844ed440105e5c8d8f51e0cffb47acf043ab9b3
Colby Miller <colbymillerdev@gmail.com>
2020-06-16 00:26:59 -0400

    Added default options to wakeDyno functions and ability to turn off logging.

--- backend/lib/wakeDyno.js
+++ backend/lib/wakeDyno.js
@@ -1,25 +1,31 @@
 const fetch = require('node-fetch');
 
-const wakeDyno = (url, interval = 29) => {
+const defaultOptions = { interval: 29, logging: true };
+
+const wakeDyno = (url, options = defaultOptions) => {
+  const { interval, logging } = options;
   const milliseconds = interval * 60000;
+
   setTimeout(() => {
     fetch(url)
-      .then(() => console.log('Successfully woke the dyno'))
-      .catch(() => console.log('Error attempting to wake the dyno'))
-      .finally(() => wakeDyno(url, interval));
+      .then(() => logging && console.log('Successfully woke the dyno'))
+      .catch(() => logging && console.log('Error attempting to wake the dyno'))
+      .finally(() => wakeDyno(url, options));
   }, milliseconds);
 };
 
-const wakeDynos = (urls, interval = 29) => {
-  const milliseconds = interval * 60000;
+const wakeDynos = (urls, options = defaultOptions) => {
+  const { interval, logging } = options;
+  const milliseconds = interval * 1;
+
   setTimeout(() => {
     if (!Array.isArray(urls)) return;
 
     const promises = urls.map((url) => fetch(url));
     Promise.all(promises)
-      .then(() => console.log('Successfully woke all dynos'))
-      .catch(() => console.log('Error attempting to wake the dynos'))
-      .finally(() => wakeDynos(urls, interval));
+      .then(() => logging && console.log('Successfully woke all dynos'))
+      .catch(() => logging && console.log('Error attempting to wake the dynos'))
+      .finally(() => wakeDynos(urls, options));
   }, milliseconds);
 };
 
